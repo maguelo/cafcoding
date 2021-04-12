ba8b48bca5ad37594d5759e88c8374744d86133c
Miguel <miguelangelmal@gmail.com>
2021-04-12 21:33:32 +0200

    Setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+'''
+@author: Cafcoding
+'''
+# !/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cafcoding',
+      version='1.1.1',
+      description='Cafcoding libraries',
+      author='Cafcoding',
+      author_email='',
+      url='',
+      packages=['cafcoding','cafcoding.tools', 'cafcoding.stages'],
+      install_requires=[],
+      )
